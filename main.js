4b5a582ffc7ff145fce521bac19115de550e1a82
Wes Bos <wesbos@gmail.com>
2022-12-08 12:35:56 -0500

    👨🏼‍🦱

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const { ipcMain } = require('electron');
 const { menubar } = require('menubar');
 
 const mb = menubar({
-	browserWindow: {
-		transparent: true,
+  browserWindow: {
+    transparent: true,
     width: 1920 / 2,
-		height:  1080 / 2,
+    height:  1080 / 2,
     alwaysOnTop: true
-	 },
-   icon: './images/hair-icon.png'
+  },
+  icon: './images/hair-icon.png'
 });
 
 mb.on('ready', () => {
-	console.log('Menubar app is ready.');
+  console.log('Menubar app is ready.');
   mb.showWindow();
   // mb.window.openDevTools();
 });
