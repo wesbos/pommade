b96685d9f04e3ebbb99d4ed048d94ea72b540817
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-12-08 12:12:02 -0800

    Dark mode

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,8 @@
 const { ipcRenderer, contextBridge } = require('electron');
 
-contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer)
+// contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer)
 
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system')
+})
