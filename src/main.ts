8a1edae11c29d5acce34a58360747780a3afb3bb
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2022-12-08 17:22:47 -0800

    TS Dark Mode

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
-import { ipcMain } from 'electron';
+import { ipcMain, nativeTheme } from 'electron';
 import { menubar } from 'menubar';
 
 const width = 960;
 const height = 540;
+
+let image = '';
+
+const createWindow = () => {
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light';
+      image = './images/hair-white-icon.png';
+    } else {
+      nativeTheme.themeSource = 'dark'
+      image = './images/hair-icon.png';
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+}
 
 const mb = menubar({
   index: `file://${__dirname}/../index.html`,
@@ -15,11 +34,12 @@ const mb = menubar({
       preload: __dirname + '/preload.js',
     }
   },
-  icon: './images/hair-icon.png'
+  icon: image
 });
 
 mb.on('ready', () => {
   console.log('Menubar app is ready.');
+  createWindow()
   mb.showWindow();
   // @ts-expect-error
   mb.window?.openDevTools();
